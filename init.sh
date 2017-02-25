d824bf93f684b4f8d0ecd0711be304fc1346d839
Dontsov Evgeny <don-and-home@mail.ru>
2017-02-25 03:02:15 +0300

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/adafruit/Adafruit_Sensor.git ./lib/Adafruit_Sensor
-cp ./lib/Adafruit_Sensor/Adafruit_Sensor.h ./lib/DHT-sensor-library/Adafruit_Sensor.h
 git clone https://github.com/adafruit/DHT-sensor-library.git ./lib/DHT-sensor-library
+cp ./lib/Adafruit_Sensor/Adafruit_Sensor.h ./lib/DHT-sensor-library/Adafruit_Sensor.h
 git clone https://github.com/milesburton/Arduino-Temperature-Control-Library.git ./lib/DallasTemperature
 git clone https://github.com/LowPowerLab/SFE_BMP180.git ./lib/SFE_BMP180
 git clone https://github.com/adafruit/Adafruit-BMP085-Library.git ./lib/BMP085
